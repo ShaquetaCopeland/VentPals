9d7a317eee9425c545fcec999764091fb1ad735c
ShaquetaCopeland <166782775+ShaquetaCopeland@users.noreply.github.com>
2025-06-23 19:00:08 -0500

    Update and rename flock.js to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+fetch('data/flock.json')
+  .then(response => response.json())
+  .then(data => {
+    const container = document.getElementById('flock-container');
+    data.forEach(bird => {
+      const card = document.createElement('div');
+      card.classList.add('bird-card');
+
+      card.innerHTML = `
+        <img src="${bird.image}" alt="${bird.name}">
+        <h3>${bird.name}</h3>
+        <p><em>${bird.title}</em></p>
+        <p>${bird.bio}</p>
+      `;
+
+      container.appendChild(card);
+    });
+  })
+  .catch(err => {
+    console.error('Error loading flock:', err);
+  });
